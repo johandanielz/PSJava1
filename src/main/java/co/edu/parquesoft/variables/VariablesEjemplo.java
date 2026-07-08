662271cdb23b11b49d18bf6333759f02e0d625f5
Johan Zuleta <johandanielz@gmail.com>
2026-07-08 11:47:20 -0500

    Ejercicio de variables

---
+++ src/main/java/co/edu/parquesoft/variables/VariablesEjemplo.java
@@ -0,0 +1,14 @@
+package co.edu.parquesoft.variables;
+
+import java.util.Scanner;
+
+public class VariablesEjemplo {
+
+    static void main() {
+        String nombre = "Johan";
+        int edad = 31;
+
+        IO.println("Hola " + nombre + " tu edad es: " + edad);
+    }
+
+}
---
+++ src/main/java/co/edu/parquesoft/variables/VariablesEjercicio.java
@@ -0,0 +1,12 @@
+package co.edu.parquesoft.variables;
+
+public class VariablesEjercicio {
+    static void main() {
+        String nombre = "Johan Daniel Zuleta Suarez";
+        int edad = 31;
+        boolean mascota = true;
+        double altura = 1.84;
+
+        IO.println("Hola soy " + nombre + " tengo " + edad + " años y mido " + altura + " ¿Tengo mascota? " + mascota);
+    }
+}
